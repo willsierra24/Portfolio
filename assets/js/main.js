61a95fb12f156afdd4eb07d2b079fcc1524c3ae9
willsierra24 <willjusi@gmail.com>
2023-03-30 23:45:16 -0500

    testimonial added

--- assets/js/main.js
+++ assets/js/main.js
@@ -88,7 +88,7 @@ modalCloses.forEach((modalClose) => {
 })
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-let swiper = new Swiper(".portfolio__container", {
+let swiperPortfolio = new Swiper(".portfolio__container", {
     cssMode: true,
     loop:true,
 
@@ -103,7 +103,17 @@ let swiper = new Swiper(".portfolio__container", {
   });
 
 /*==================== TESTIMONIAL ====================*/
+let swiperTestimonial = new Swiper(".testimonial__container", {
+    loop:true,
+    grabCursor: true,
+    spaceBetween:48,
 
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+  });
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
